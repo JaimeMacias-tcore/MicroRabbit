8ebe0b42f40a08192d6f6cd93e5e01f4d2de5c65
Jaime Macias <jaime.macias@transcore.com>
2025-06-23 13:42:39 -0600

    Implemented interfaces and classes for domain core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent: Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+        // Marker interface for event handlers
+    }
